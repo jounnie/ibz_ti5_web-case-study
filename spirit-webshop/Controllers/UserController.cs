d75fc2fd7297e321dd19c3b54350093395731b7c
Jonathan Ruiz <jonathan.ruiz@gateb.com>
2019-03-07 19:05:43 +0100

    users: add get, create, update and delete methods

---
+++ spirit-webshop/Controllers/UserController.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using spirit_webshop.Entities;
+
+namespace spirit_webshop.Controllers
+{
+    [Route("api/users")]
+    public class UserController : Controller
+    {
+        private readonly SpiritDbContext _context;
+
+        public UserController(SpiritDbContext context)
+        {
+            _context = context;
+        }
+
+        /*[HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            return await _context.User
+                //.Include(User => User.UserCategories)
+                .ToListAsync();
+        }*/
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> Get(int id)
+        {
+            var result = await _context.User.FindAsync(id);
+
+            if (result == null) return NotFound();
+
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Create([FromBody] User item)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            _context.User.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new {id = item.Id}, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(long id, [FromBody] User item)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            if (id != item.Id) return BadRequest("request id and item id not equal");
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _context.User.FindAsync(id);
+
+            if (user == null) return NotFound();
+
+            _context.User.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
